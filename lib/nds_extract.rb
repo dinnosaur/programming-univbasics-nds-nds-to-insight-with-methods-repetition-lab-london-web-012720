25b9ba05cbadf486168143f685e69a17f4a67046
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-25 18:43:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,6 +49,20 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+final_total =0 
+total_index =0 
+
+
+
+
+while total_index < source.length do 
+     final_total += directors_totals[total_index][:name]
+   end
+puts final_total
+   final_total 
+  
+
+  
 
 
 
